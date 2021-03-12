c4ba2b162223352d1d306e639bed8ac792abe609
ReetBarik <reetbarik94@gmail.com>
2021-03-11 17:37:26 -0800

    Edge probability visualization script created

--- Population_Dataset/Edgelists/EdgeProbability.py
+++ Population_Dataset/Edgelists/EdgeProbability.py
@@ -9,6 +9,7 @@ import os
 os.chdir('C:/Users/reetb/Desktop/Covasim_PREEMPT/Population_Dataset/Edgelists/')
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = []
 
@@ -26,10 +27,25 @@ probs = pd.DataFrame(columns=columns)
 for i in range(0,13):
     probs[columns[i]] = df[i][2]
     
+ylim = max(max(probs['V0']), max(probs['V1']), max(probs['V2']), max(probs['V3']), max(probs['V4']), max(probs['V5']), max(probs['V6']), max(probs['V8']), max(probs['V9']), max(probs['V10']), max(probs['V11']), max(probs['V12']))
     
-for i in range(1,13):
-    print(probs['V0'].equals(probs[columns[i]]))
     
+#for i in range(1,13):
+#    print(probs['V0'].equals(probs[columns[i]]))
 
     
-    
+fig, ax = plt.subplots(3, 4, sharex='col', sharey='row')
+
+for i in range(3):
+    for j in range(4):
+        idx = 2 * i + j
+        l = list(sorted(probs['V' + str(idx)]))
+        ax[i, j].set_ylim(0, 1)
+        ax[i, j].plot(l)
+        
+        
+fig.suptitle('Edge Probability distributions', fontsize=15)
+fig.text(0.5, 0.03, 'Edges', ha='center')
+fig.text(0.04, 0.5, 'Probability', va='center', rotation='vertical')
+        
+plt.savefig('Probabilities.png', dpi = 500)
